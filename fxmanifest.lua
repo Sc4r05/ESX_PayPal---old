8d76385e25f29c9bc230a182c43a81443b0a253e
Scar <35073672+Sc4r05@users.noreply.github.com>
2023-08-06 17:37:30 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'bodacious'
+games { 'rdr3', 'gta5' }
+
+client_script('client.lua')
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    'server.lua'
+}
+
+ui_page('html/index.html')
+
+files {
+    'html/index.html',
+    'html/script.js',
+    'html/style.css',
+    'html/img/logo.png',
+    'html/img/logo-red.png',
+    'html/img/logo-blue.png',
+}
